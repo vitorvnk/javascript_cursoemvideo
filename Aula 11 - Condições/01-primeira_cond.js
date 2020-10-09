8a3526b7ab8a1bf2d67636314d0e36206ebdb2f6
Vítor Alexandre <62453489+vitorvnk@users.noreply.github.com>
2020-10-09 19:47:28 -0300

    .

---
+++ Aula 11 - Condições/01-primeira_cond.js
@@ -0,0 +1,11 @@
+var vel = 4
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if(vel > 60){ 
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+else{
+    console.log(`Você é uma banana!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
